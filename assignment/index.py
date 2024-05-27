1f6b0c4e03596637d984b5bc09aab3fccd368443
alamulord <abdulsamadaayomide00@gmail.com>
2024-05-27 17:32:49 +0100

    my first python project

---
+++ assignment/index.py
@@ -0,0 +1,28 @@
+a = 2022
+print (a)
+
+# modifying the fist variable by changing the value first before multiplying
+a = 2024
+print (a)
+b = 1
+print (a+b)
+
+# Calculating the numbers of mins in a week with variable
+daysPerWeek = 7
+hoursPerDay = 24
+minutesPerDay = 1440 
+# To calculate the number of mins in a week i'll multiply the number of days in a week with the number of mins per hour in a day
+minutesPerWeek = minutesPerDay * daysPerWeek
+print (minutesPerWeek, 'minutes in a week')
+
+# How many mins would there be if we were using 26 hours in a week 
+hoursPerDay = 26
+minutesPerHour = 60
+minutesPerWeek = hoursPerDay * minutesPerHour
+print (minutesPerWeek, 'minutes in a week')
+
+# write a short program to print three lines of name, DOB, and favorite color
+about = ('My name is Abdulsamad, '
+'i was born in the year 8th August 2012, '
+'My favorite color is Pink')
+print (about)
