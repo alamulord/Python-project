1477a472a9f60f1007b7d4e6e93001f66d3ca6ec
alamulord <abdulsamadaayomide00@gmail.com>
2024-05-29 15:30:50 +0100

    initial commit

--- assignment/index.py
+++ assignment/index.py
@@ -5,13 +5,13 @@ print (a)
 a = 2024
 print (a)
 b = 1
-print (a+b)
+print (a*b)
 
 # Calculating the numbers of mins in a week with variable
 daysPerWeek = 7
 hoursPerDay = 24
 minutesPerDay = 1440 
-# To calculate the number of mins in a week i'll multiply the number of days in a week with the number of mins per hour in a day
+# To calculate the number of mins in a week i'll multiply the number of days in a week with the number of mins per day in a day
 minutesPerWeek = minutesPerDay * daysPerWeek
 print (minutesPerWeek, 'minutes in a week')
 
@@ -23,6 +23,6 @@ print (minutesPerWeek, 'minutes in a week')
 
 # write a short program to print three lines of name, DOB, and favorite color
 about = ('My name is Abdulsamad, '
-'i was born in the year 8th August 2012, '
-'My favorite color is Pink')
+'i was born in the year 24th may 2024, '
+'my favorite color is Pink')
 print (about)
